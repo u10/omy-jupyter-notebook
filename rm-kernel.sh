b6ca7a7c9cb6295111c92b80f9b327b8e52b22a4
vaniship <vaniship@gmail.com>
2019-07-13 00:25:35 +0800

    refactor: refactor kernel remove script

--- rm-kernel.sh
+++ rm-kernel.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 
-if [ -e ~/Library/Jupyter/kernels/$1 ]; then
-  read -t 5 -p "remove kernels: $1 ? (y/n): " ok
-  ok=${ok:-'n'}
-  if [ $ok = 'y' -o $ok = 'Y' ]; then
-    rm -r ~/Library/Jupyter/kernels/$1
-    echo remove kernels: $1 success.
-  else
-    echo remove skiped.
-  fi
-else
-  echo Kernels $1 is not exist.
-fi
-echo '-------'
-echo 'Now kernel list:'
-ls -l ~/Library/Jupyter/kernels|awk '{if(NR>1)print $NF}'
+runPath=$(cd "$(dirname "$0")"; pwd)
+pythonInstallPath=${runPath}/python-runtime
+
+source ${pythonInstallPath}/bin/activate
+
+jupyter kernelspec remove $1
+jupyter kernelspec list
