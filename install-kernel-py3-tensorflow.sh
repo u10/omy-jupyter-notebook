f247cf3149eb2ae020cd716c0e82f9504b02e864
vaniship <vaniship@gmail.com>
2019-07-25 17:29:35 +0800

    chore: add lib

--- install-kernel-py3-tensorflow.sh
+++ install-kernel-py3-tensorflow.sh
@@ -13,13 +13,14 @@ function kernel_install {
 
   source ${installPath}/bin/activate
 
-  pip install --upgrade tensorflow
+  pip install --upgrade tensorflow pandas numpy matplotlib seaborn keras
   pip install --force-reinstall ipykernel
 
   python -m ipykernel install --user --name=py3-tensorflow
 }
 
-if [ $1 = '-t' ];then
+arg1=$1
+if [ ${arg1:-''} = '-t' ];then
   if [ -e $installPath ]; then
     cd ${installPath}
     source ${installPath}/bin/activate
